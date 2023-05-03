c52efe6f4779dec30ae8ac3b530f3f8bc6cd7ad0
SimonElkhojHess <simo85j7@stud.kea.dk>
2023-05-03 13:52:20 +0200

    First Commit

---
+++ src/main/java/dk/kea/calendue/CalendueApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.calendue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendueApplication.class, args);
+    }
+
+}
