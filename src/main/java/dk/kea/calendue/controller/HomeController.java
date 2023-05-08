66bc40ae16ecc624590d41c2c1e950c9b17678d6
simo75m1 <simo75m1@stud.kea.dk>
2023-05-08 14:05:34 +0200

    create Login html

--- src/main/java/dk/kea/calendue/controller/HomeController.java
+++ src/main/java/dk/kea/calendue/controller/HomeController.java
@@ -4,6 +4,7 @@ import dk.kea.calendue.model.User;
 import dk.kea.calendue.repository.UserRepository;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,6 +17,15 @@ public class HomeController {
         this.userRepo = userRepo;
     }
 
+    @GetMapping("/login")
+    public String showLoginPage(HttpSession session)
+    {
+        if(session.getAttribute("user_id") == null)
+        {
+            return "/login";
+        }
+        return "redirect:/";
+    }
     @PostMapping("/login")
     public String tryLogin(@RequestParam String username, @RequestParam String password, HttpSession session)
     {
@@ -33,5 +43,16 @@ public class HomeController {
         }
         session.setAttribute("wronglogin", true);
         return "/login";
+    }
+
+    @GetMapping("/logout")
+    public String tryLogout(HttpSession session)
+    {
+        session.removeAttribute("user_id");
+        session.removeAttribute("username");
+        session.removeAttribute("email");
+        session.removeAttribute("is_admin");
+
+        return "redirect:/login";
     }
 }
