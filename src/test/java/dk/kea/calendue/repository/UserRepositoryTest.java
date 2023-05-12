5f7366d46719be25b08dc4885ca66d74ff758e3d
simo75m1 <simo75m1@stud.kea.dk>
2023-05-12 13:26:42 +0200

    unit test for login branch

---
+++ src/test/java/dk/kea/calendue/repository/UserRepositoryTest.java
@@ -0,0 +1,54 @@
+package dk.kea.calendue.repository;
+
+import dk.kea.calendue.model.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserRepositoryTest(UserRepository userRepository)
+    {
+        this.userRepository = userRepository;
+    }
+
+    @BeforeAll
+    void setUp()
+    {
+    }
+
+    @BeforeEach
+    void setUpEach()
+    {}
+
+    @Test
+    void testCheckLoginWithValidCredentials() {
+        userRepository = new UserRepository();
+        //arrange
+        String username = "user";
+        String password = "user";
+
+        //act
+        boolean result = userRepository.checkLogin(username, password);
+
+        //assert
+        assertEquals(true, result);
+    }
+
+    //void testCheckLoginWithInvalidCredentials() {}
+}
