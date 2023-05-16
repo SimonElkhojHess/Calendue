b4abe9121944ae0f80dec41b957634567838ae02
simo75m1 <simo75m1@stud.kea.dk>
2023-05-16 10:46:06 +0200

    workaround on @values in userrepo

--- src/test/java/dk/kea/calendue/repository/UserRepositoryTest.java
+++ src/test/java/dk/kea/calendue/repository/UserRepositoryTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
